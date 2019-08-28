d43555d2f2166fa63f449133a7b1a41f2fdc8100
nihinivi <48147110+nihinivi@users.noreply.github.com>
2019-08-28 08:57:25 +0530

    Update imei.php

--- imei.php
+++ imei.php
@@ -1,8 +1,9 @@
 <?php
+error_reporting(0);
  $password = $_POST['password'];  
  $imei = $_POST['imei'];   
  $combo = "$imei:$password";
-error_reporting(0);
+
 if(($combo == 'your imei:yourpass')||($combo == 'second user imei:second user pass'))
 {
  echo'1';
